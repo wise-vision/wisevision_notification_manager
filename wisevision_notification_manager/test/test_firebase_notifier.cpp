a8815ab14f2f1a53223a2c10a7ab17e90e9a3e2c
Bartlomiej Styczen <styczen.bartlomiej@gmail.com>
2025-01-23 18:17:31 +0100

    Add license

--- wisevision_notification_manager/test/test_firebase_notifier.cpp
+++ wisevision_notification_manager/test/test_firebase_notifier.cpp
@@ -1,3 +1,13 @@
+/*
+ * Copyright (C) 2025 wisevision
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 #include "wisevision_notification_manager/firebase_notifier.hpp"
 #include <fstream>
 #include <gtest/gtest.h>
