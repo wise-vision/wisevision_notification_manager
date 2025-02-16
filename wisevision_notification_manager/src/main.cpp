b3471d636ef318cb034e65199a98cae4010cb72d
Khasreto <adam-krawczyk@outlook.com>
2025-02-16 19:56:51 +0100

    Stabilization/2501 (#4)
    * Updated dockerfile and ci files (#2)
    
    * Updated dockerfile and ci files
    
    * Added workspace clean up to ci
    
    * switched workspace name
    
    * Updated path name
    
    * Udpated build directory
    
    * Add license (#3)
    
    * Updated to wisevision runner (#5)
    
    ---------
    
    Co-authored-by: czarekk215 <128484058+czarekk215@users.noreply.github.com>
    Co-authored-by: Bartlomiej Styczen <styczen.bartlomiej@gmail.com>

--- wisevision_notification_manager/src/main.cpp
+++ wisevision_notification_manager/src/main.cpp
@@ -1,3 +1,13 @@
+/*
+ * Copyright (C) 2025 wisevision
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 // Copyright (c) 2024, WiseVision. All rights reserved.
 #include <wisevision_notification_manager/notification_handler.hpp>
 
